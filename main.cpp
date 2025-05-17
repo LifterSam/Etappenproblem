f6b6bdd1df266df991061c401cd00b2024a3ebd9
LifterSam <audioropolis@gmx.de>
2025-05-17 19:58:47 +0200

    reading input.txt w. relative f-path & control output

--- main.cpp
+++ main.cpp
@@ -1,9 +1,41 @@
 #include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
 
-using namespace std;
+int main() {
+    std::ifstream inputDatei("../../input/input.txt");
+     // Datei öffnen
 
-int main()
-{
-    cout << "Hello World!" << endl;
+    if (!inputDatei.is_open()) {
+        std::cerr << "Konnte die Datei nicht oeffnen!" << std::endl;
+        return 1;
+    }
+
+    int etappen;
+    int tage;
+    int zahlImStream;
+    std::vector<int> etappenLaengen;
+
+    inputDatei >> etappen;
+    inputDatei >> tage;
+    while (inputDatei >> zahlImStream) {
+        etappenLaengen.push_back(zahlImStream);
+    }
+
+    std::cout << "Eingelesene Etappen: " << etappen << std::endl;
+    std::cout << "Eingelesene Tage: " << tage << std::endl;
+
+
+    std::cout << "Eingelesene EtappenLaengen: [";
+    if (!etappenLaengen.empty()) {
+        for (size_t i = 0; i < etappenLaengen.size() - 1; ++i) {
+            std::cout << etappenLaengen[i] << ", ";
+        }
+        std::cout << etappenLaengen.back();
+    }
+    std::cout << "]" << std::endl;
+
+    inputDatei.close(); // Datei schließen
     return 0;
 }
